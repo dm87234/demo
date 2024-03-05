bd85d4a87cc89bb1df0f6a8a36ec9511a9ad6b71
SuperIce <dm872345@gmail.com>
2024-03-05 18:52:01 +0800

    demo

--- demo.js
+++ demo.js
@@ -2,18 +2,22 @@
   "use strict"
   const ghost = document.querySelector(".ghost")
   let scrollTimer = null
+  let flag = true
   window.addEventListener("scroll", () => {
     clearTimeout(scrollTimer)
-    let n = document.documentElement.scrollTop
-    scrollTimer = setTimeout(() => {
-      console.log(n)
-      if (n > 100 && n < 200) {
-        ghost.style.left = 0
-        ghost.style.opacity = 1
-      } else if (n > 200) {
-        ghost.style.left = "100vw"
-        ghost.style.opacity = 0
-      }
-    }, 10)
+    if (flag) {
+      let n = document.documentElement.scrollTop
+      scrollTimer = setTimeout(() => {
+        console.log(n)
+        if (n > 100 && n < 200) {
+          ghost.style.left = 0
+          ghost.style.opacity = 1
+        } else if (n > 200) {
+          ghost.style.left = "100vw"
+          ghost.style.opacity = 0
+          flag = false
+        }
+      }, 10)
+    }
   })
 })()
